9f4af80a62cf97cd19520d14b7627498ea1365aa
kim <yj@kimui-MacBookPro.local>
2026-07-30 10:54:23 +0900

    Add Vercel deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript:
+    process.env.NEXT_BUILD_TARGET === "vercel"
+      ? { tsconfigPath: "tsconfig.vercel.json" }
+      : undefined,
 };
 
 export default nextConfig;
